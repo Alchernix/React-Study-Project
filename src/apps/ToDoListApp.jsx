d18ef7a10621e9ff6f6f6eaf797cb61544ff7564
cloudy0522 <cloudy0522@outlook.com>
2025-06-04 19:21:23 +0900

    투두리스트- 작업중중

--- src/apps/ToDoListApp.jsx
+++ src/apps/ToDoListApp.jsx
@@ -1,10 +1,36 @@
 import { useState, useRef, useEffect} from "react";
 
+//todo: 분류 기능/정렬
 export default function ToDoListApp() {
- const [toDoList, setToDoList] = useState([])
+    const [toDoList, setToDoList] = useState([])
+    const inputText = useRef(null)
+
+    function addToDo(){
+        let newItem = {
+            id : Math.max(toDoList.map(item => item.id)),
+            title: inputText.current.value,
+            finished: false,
+        }
+        setToDoList((prev) => [...prev, newItem])
+    }
+
+    function deleteToDo(id){
+        setToDoList((prev) => prev.filter(item => item.id != id))
+    }
 
  return(
     <div>
+        <ul>
+            {toDoList.map(item =>
+            <li key = {item.id}>
+                {item.title}
+            </li>
+            )}
+        </ul>
+        <form>
+            <input type="text" ref={inputText}></input>
+            <button id="add" onClick={addToDo}></button>
+        </form>
         <p>작업예정</p>
     </div>
  )
